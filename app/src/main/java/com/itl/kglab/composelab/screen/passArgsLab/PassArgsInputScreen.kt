8a628db019dad707aab60af33f1d48e71a412eab
KenLiau <itl.kglab@gmail.com>
2022-06-26 16:03:14 +0800

    PassArgsLab修正跳轉畫面狀態遺失的問題

--- app/src/main/java/com/itl/kglab/composelab/screen/passArgsLab/PassArgsInputScreen.kt
+++ app/src/main/java/com/itl/kglab/composelab/screen/passArgsLab/PassArgsInputScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -16,15 +17,25 @@ import com.itl.kglab.composelab.screen.passArgsLab.PassArgsScreen.RECEIVE_SCREEN
 /**
  *  使用Navigation切換畫面，且使用args傳遞參數。
  *
- *  參考資料： https://developer.android.com/jetpack/compose/navigation
+ *  參考資料：
+ *      Navigation -
+ *          https://developer.android.com/jetpack/compose/navigation
+ *      儲存UI狀態 -
+ *          https://developer.android.com/jetpack/compose/state#restore-ui-state
  */
 
 @Composable
 fun PassArgsInputScreen(
     navController: NavController
 ) {
+    // 跳轉畫面無需儲存的資料使用remember
     var textFieldState by remember {
         mutableStateOf("")
+    }
+    // 跳轉畫面需要儲存狀態則使用rememberSaveable
+    // 僅使用remember時狀態會在跳轉畫面刷新而遺失
+    var timeStampState by rememberSaveable {
+        mutableStateOf("尚未紀錄時間")
     }
 
     Column(
@@ -46,9 +57,17 @@ fun PassArgsInputScreen(
             modifier = Modifier.fillMaxWidth()
         )
         Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = timeStampState,
+            maxLines = 1,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(8.dp))
         Button(onClick = {
             // 由於參數是轉換成URL的方式在傳遞，因此字串不得為空字串，否則會產生路徑解析的問題
-            val args = InputArgs(textFieldState, System.currentTimeMillis())
+            val time = System.currentTimeMillis()
+            timeStampState = "上次記錄時間：${time}"
+            val args = InputArgs(textFieldState, time)
 //            navigateWithSimpleURL(navController = navController, args)
             navigateWithURL(navController, args)
         }) {
